82737a4c54c57eea75a2ca408f89a40aee0220a3
vasylluk <vasyl3luk@gmail.com>
2018-06-12 20:07:39 +0300

    all in one

---
+++ ArraySort_Methods/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace ArraySort_Methods
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fillButton = new System.Windows.Forms.Button();
+            this.ShowCheckBox = new System.Windows.Forms.CheckBox();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.BFillButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BCountUpDown = new System.Windows.Forms.NumericUpDown();
+            this.BListBox = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ShowBCheckBox = new System.Windows.Forms.CheckBox();
+            this.read1Button = new System.Windows.Forms.Button();
+            this.read2Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.countUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BCountUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // countUpDown
+            // 
+            this.countUpDown.Location = new System.Drawing.Point(353, 74);
+            this.countUpDown.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.countUpDown.Name = "countUpDown";
+            this.countUpDown.Size = new System.Drawing.Size(120, 20);
+            this.countUpDown.TabIndex = 0;
+            this.countUpDown.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Кількість елементів:";
+            // 
+            // fillButton
+            // 
+            this.fillButton.Location = new System.Drawing.Point(353, 100);
+            this.fillButton.Name = "fillButton";
+            this.fillButton.Size = new System.Drawing.Size(75, 23);
+            this.fillButton.TabIndex = 2;
+            this.fillButton.Text = "Заповнити";
+            this.fillButton.UseVisualStyleBackColor = true;
+            this.fillButton.Click += new System.EventHandler(this.fillButton_Click);
+            // 
+            // ShowCheckBox
+            // 
+            this.ShowCheckBox.AutoSize = true;
+            this.ShowCheckBox.Checked = true;
+            this.ShowCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowCheckBox.Location = new System.Drawing.Point(13, 37);
+            this.ShowCheckBox.Name = "ShowCheckBox";
+            this.ShowCheckBox.Size = new System.Drawing.Size(121, 17);
+            this.ShowCheckBox.TabIndex = 3;
+            this.ShowCheckBox.Text = "Показувати масив";
+            this.ShowCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // sortButton
+            // 
+            this.sortButton.Enabled = false;
+            this.sortButton.Location = new System.Drawing.Point(13, 174);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(75, 23);
+            this.sortButton.TabIndex = 5;
+            this.sortButton.Text = "Сортувати";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Масив:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Методи сотрування:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 8;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Швидке",
+            "Вставками",
+            "Злиття",
+            "Вибіркове",
+            "Шелла"});
+            this.checkedListBox1.Location = new System.Drawing.Point(194, 74);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Enabled = false;
+            this.confirmButton.Location = new System.Drawing.Point(194, 174);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
+            this.confirmButton.TabIndex = 10;
+            this.confirmButton.Text = "ОК";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Час роботи(ms):";
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(14, 383);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.timeTextBox.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Відсортований масив:";
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.Location = new System.Drawing.Point(14, 262);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(120, 95);
+            this.resultListBox.TabIndex = 11;
+            // 
+            // BFillButton
+            // 
+            this.BFillButton.Location = new System.Drawing.Point(194, 290);
+            this.BFillButton.Name = "BFillButton";
+            this.BFillButton.Size = new System.Drawing.Size(75, 23);
+            this.BFillButton.TabIndex = 17;
+            this.BFillButton.Text = "Заповнити";
+            this.BFillButton.UseVisualStyleBackColor = true;
+            this.BFillButton.Visible = false;
+            this.BFillButton.Click += new System.EventHandler(this.BFillButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(190, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(160, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Кількість елементів масиву В:";
+            this.label6.Visible = false;
+            // 
+            // BCountUpDown
+            // 
+            this.BCountUpDown.Location = new System.Drawing.Point(194, 264);
+            this.BCountUpDown.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.BCountUpDown.Name = "BCountUpDown";
+            this.BCountUpDown.Size = new System.Drawing.Size(120, 20);
+            this.BCountUpDown.TabIndex = 15;
+            this.BCountUpDown.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.BCountUpDown.Visible = false;
+            // 
+            // BListBox
+            // 
+            this.BListBox.FormattingEnabled = true;
+            this.BListBox.Location = new System.Drawing.Point(194, 385);
+            this.BListBox.Name = "BListBox";
+            this.BListBox.Size = new System.Drawing.Size(120, 95);
+            this.BListBox.TabIndex = 19;
+            this.BListBox.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(190, 367);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Масив B:";
+            this.label7.Visible = false;
+            // 
+            // ShowBCheckBox
+            // 
+            this.ShowBCheckBox.AutoSize = true;
+            this.ShowBCheckBox.Checked = true;
+            this.ShowBCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowBCheckBox.Location = new System.Drawing.Point(194, 348);
+            this.ShowBCheckBox.Name = "ShowBCheckBox";
+            this.ShowBCheckBox.Size = new System.Drawing.Size(131, 17);
+            this.ShowBCheckBox.TabIndex = 20;
+            this.ShowBCheckBox.Text = "Показувати масив B";
+            this.ShowBCheckBox.UseVisualStyleBackColor = true;
+            this.ShowBCheckBox.Visible = false;
+            // 
+            // read1Button
+            // 
+            this.read1Button.Location = new System.Drawing.Point(353, 129);
+            this.read1Button.Name = "read1Button";
+            this.read1Button.Size = new System.Drawing.Size(75, 23);
+            this.read1Button.TabIndex = 21;
+            this.read1Button.Text = "Зчитати";
+            this.read1Button.UseVisualStyleBackColor = true;
+            this.read1Button.Click += new System.EventHandler(this.read1Button_Click);
+            // 
+            // read2Button
+            // 
+            this.read2Button.Location = new System.Drawing.Point(194, 319);
+            this.read2Button.Name = "read2Button";
+            this.read2Button.Size = new System.Drawing.Size(75, 23);
+            this.read2Button.TabIndex = 22;
+            this.read2Button.Text = "Зчитати";
+            this.read2Button.UseVisualStyleBackColor = true;
+            this.read2Button.Visible = false;
+            this.read2Button.Click += new System.EventHandler(this.read2Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 492);
+            this.Controls.Add(this.read2Button);
+            this.Controls.Add(this.read1Button);
+            this.Controls.Add(this.ShowBCheckBox);
+            this.Controls.Add(this.BListBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.BFillButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BCountUpDown);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.ShowCheckBox);
+            this.Controls.Add(this.fillButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.countUpDown);
+            this.Name = "Form1";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.countUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BCountUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown countUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button fillButton;
+        private System.Windows.Forms.CheckBox ShowCheckBox;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.Button BFillButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown BCountUpDown;
+        private System.Windows.Forms.ListBox BListBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox ShowBCheckBox;
+        private System.Windows.Forms.Button read1Button;
+        private System.Windows.Forms.Button read2Button;
+    }
+}
+
---
+++ ArraySort_Methods/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraySort_Methods
+{
+    public partial class Form1 : Form
+    {
+
+        public long[] arr;
+        public long[] arrB;
+        public long[] c;
+        public string method;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ReadFromFile(ListBox l1, long[] array)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count > 1 || checkedListBox1.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("Виберіть рівно один метод", "Помилка");
+            }
+            else
+            {
+                method = Convert.ToString(checkedListBox1.CheckedItems[0]);
+                if (method == "Злиття")
+                {
+                    label6.Visible = true;
+                    BCountUpDown.Visible = true;
+                    BFillButton.Visible = true;
+                    BListBox.Visible = true;
+                    label7.Visible = true;
+                    read2Button.Visible = true;
+                    ShowBCheckBox.Visible = true;
+                    sortButton.Enabled = false;
+                    fillButton.Enabled = false;
+                }
+                else
+                {
+                    sortButton.Enabled = true;
+                    fillButton.Enabled = false;
+                }
+            }
+        }
+
+        private void fillButton_Click(object sender, EventArgs e)
+        {
+            arr = new long[(long)countUpDown.Value];
+            if (ShowCheckBox.Checked == true)
+            {
+                Random r = new Random();
+                for (int i = 0; i < countUpDown.Value; i++)
+                {
+                    arr[i] = r.Next(-1000000000,1000000000);
+                }
+                listBox1.DataSource = arr;
+            }
+            else
+            {
+                Random r = new Random();
+                for (int i = 0; i < countUpDown.Value; i++)
+                {
+                    arr[i] = r.Next(-1000000000, 1000000000);
+                }
+            }
+            confirmButton.Enabled = true;
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            double t = 0;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            if (method == "Швидке")
+            {
+                watch = System.Diagnostics.Stopwatch.StartNew();
+                int left = 0, right = arr.Length - 1; 
+                if (left > right || left < 0 || right < 0) return;
+
+                int index = partition(arr, left, right);
+
+                if (index != -1)
+                {
+                    quickSort(arr, left, index - 1);
+                    quickSort(arr, index + 1, right);
+                }
+                resultListBox.DataSource = arr;
+            }
+
+            if (method == "Вставками")
+            {
+                watch = System.Diagnostics.Stopwatch.StartNew();
+                long і, j, newValue;
+                for (і = 1; і < arr.Length; і++)
+                {
+                    newValue = arr[і]; j = і;
+                    while (j > 0 && arr[j - 1] > newValue)
+                    {
+                        arr[j] = arr[j - 1];
+                        j--;
+                    }
+                    arr[j] = newValue;
+                }
+                resultListBox.DataSource = arr;
+            }
+
+            if (method == "Злиття")
+            {
+                long i, n = 2*arr.Length;
+                c = new long[n];
+                for (i = 0; i < n/2; i++)
+                {
+                    if (arr[i] > arrB[i])
+                    {
+                        c[i + i] = arrB[i];
+                        c[i + i + 1] = arr[i];
+                    }
+                    else
+                    {
+                        c[i + i] = arr[i];
+                        c[i + i + 1] = arrB[i];
+                    }
+                }
+                for (int j = 0; j < n - 1; j++)
+                {
+                    for (int k = j + 1; k < n; k++)
+                    {
+                        long tmp = c[j];
+                        if (c[j] > c[k])
+                        {
+                            c[j] = c[k];
+                            c[k] = tmp;
+                        }
+                    }
+                }
+                resultListBox.DataSource = c;
+            }
+            if (method == "Вибіркове")
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    long pos_min = i;
+
+                    for (int j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[j] < arr[pos_min])
+                        {
+                            pos_min = j;
+                        }
+                    }
+                    if (pos_min != i)
+                    {
+                        long temp = arr[i];
+                        arr[i] = arr[pos_min];
+                        arr[pos_min] = temp;
+                    }
+                }
+                resultListBox.DataSource = arr;
+            }
+            if (method == "Шелла")
+            {
+                long step = arr.Length / 2;
+                while (step>0)
+                {
+                    for (int i = 0; i < arr.Length - step; i++)
+                    {
+                        int j = i;
+                        while (j>=0 && arr[j]>arr[j+step])
+                        {
+                            long temp = arr[j];
+                            arr[j] = arr[j+step];
+                            arr[j+step] = temp;
+                            j--;
+                        }
+                    }
+                    step /= 2;
+                }
+                resultListBox.DataSource = arr;
+            }
+            watch.Stop();
+            t = watch.ElapsedMilliseconds;
+            if(t>=1)
+                timeTextBox.Text = Convert.ToString(t);
+            else
+                timeTextBox.Text = " <1";
+        }
+
+        public static void quickSort(long[] A, int left, int right)
+        {
+            if (left > right || left < 0 || right < 0) return;
+
+            int index = partition(A, left, right);
+
+            if (index != -1)
+            {
+                quickSort(A, left, index - 1);
+                quickSort(A, index + 1, right);
+            }
+        }
+
+        private static void swap(long[] A, int left, int right)
+        {
+            long tmp = A[left];
+            A[left] = A[right];
+            A[right] = tmp;
+        }
+
+
+        private static int partition(long[] A, int left, int right)
+        {
+            if (left > right) return -1;
+            int end = left;
+            long p = A[right];
+            for (int i = left; i < right; i++)
+            {
+                if (A[i] < p)
+                {
+                    swap(A, i, end);
+                    end++;
+                }
+            }
+            swap(A, end, right);
+            return end;
+        }
+
+        private void BFillButton_Click(object sender, EventArgs e)
+        {
+            arrB = new long[(long)BCountUpDown.Value];
+            if (arr.Length != arrB.Length)
+            {
+                MessageBox.Show("Масиви неодинакової розмірності", "Помилка");
+            }
+            if (ShowBCheckBox.Checked == true)
+            {
+                Random r = new Random();
+                for (int i = 0; i < BCountUpDown.Value; i++)
+                {
+                    arrB[i] = r.Next(-1000000000, 1000000000);
+                }
+                BListBox.DataSource = arrB;
+            }
+            else
+            {
+                Random r = new Random();
+                for (int i = 0; i < BCountUpDown.Value; i++)
+                {
+                    arrB[i] = r.Next(-1000000000, 1000000000);
+                }
+            }
+            sortButton.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void read1Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                using (StreamReader r = new StreamReader(f.OpenFile()))
+                {
+
+                    string[] words = Regex.Split(r.ReadToEnd(), @"\s+");
+                    arr = new long[(long)words.Length];
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        arr[i] = Convert.ToInt64(words[i]);
+                    }
+                    listBox1.DataSource = arr;
+                }
+            }
+            fillButton.Enabled = false;
+            confirmButton.Enabled = true;
+        }
+
+        private void read2Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f1 = new OpenFileDialog();
+            if (f1.ShowDialog() == DialogResult.OK)
+            {
+                BListBox.Items.Clear();
+                using (StreamReader r = new StreamReader(f1.OpenFile()))
+                {
+                    string[] words1 = Regex.Split(r.ReadToEnd(), @"\s+");
+                    arrB = new long[(long)words1.Length];
+                    for (int i = 0; i < words1.Length; i++)
+                    {
+                        arrB[i] = Convert.ToInt64(words1[i]);
+                    }
+                    BListBox.DataSource = arrB;
+                }
+            }
+            BFillButton.Enabled = false;
+            confirmButton.Enabled = true;
+            sortButton.Enabled = true;
+        }
+    }
+}
